0139eb8bee8d48a7ad463455db5a86ebacd122ea
Yiying Zou <yiyingzou@gmail.com>
2019-12-05 11:03:45 -0500

    added auto suggestion for input

--- src/App.js
+++ src/App.js
@@ -6,42 +6,44 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      userInput: ""
+      userInput: "",
+      autocompleteWorking: false,
+      movieSuggestions: []
     };
   }
 
-  componentDidMount() {
-    this.getMovieDetails(this.state.userInput);
-  }
-
-  getMovieDetails = userInput => {
-    setTimeout(() => {
+  getMovieDetails = () => {
+    if (this.state.userInput.length > 0) {
+      console.log("getMovieDetails ran");
       axios({
         url: `https://api.themoviedb.org/3/search/movie`,
         method: "GET",
         dataResponse: "json",
         params: {
           api_key: `38f9a8f5c677f0356adca226f357b762`,
-          query: userInput,
+          query: this.state.userInput,
           include_adult: false
         }
       }).then(response => {
+        console.log(this.state.userInput);
         console.log(response);
-      });
-    }, 2000);
+        console.log(response.data.results);
+
+        this.setState({
+          autocompleteWorking: true,
+          movieSuggestions: response.data.results
+        });
+      }); // end of .then
+    }
   };
 
   //This function gets users search term as they type.
   getUserInput = e => {
     console.log("yay movies");
-    console.log(e.target.id);
     console.log(e.target.value);
     this.setState({
-      return { [e.target.id]: e.target.value }
-    }),
-      () => {
-        console.log(this.getMovieDetails(this.state.userInput));
-      };
+      [e.target.id]: e.target.value
+    });
   };
 
   render() {
@@ -52,9 +54,24 @@ class App extends Component {
           type="text"
           id="userInput"
           value={this.state.userInput}
-          onChange={this.getUserInput}
+          onChange={e => {
+            this.getUserInput(e);
+            this.getMovieDetails();
+          }}
         />
         <button type="submit">Search Movie</button>
+        {this.state.autocompleteWorking
+          ? this.state.movieSuggestions.map((movieSuggestion, i) => {
+              const movieYear = movieSuggestion.release_date.slice(0, 4);
+              return (
+                <li key={i}>
+                  <p>
+                    {movieSuggestion.title} ({movieYear})
+                  </p>
+                </li>
+              );
+            })
+          : null}
       </div>
     );
   }
