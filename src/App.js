1b059c653b4155056cd19ded9f370c21c1e8399b
Andrew Alexander <aarubesa@gmail.com>
2019-12-05 15:14:38 -0500

    removed accidental import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
-import { networkInterfaces } from "os";
 
 class App extends Component {
   constructor() {
