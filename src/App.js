a4da110f68fd3161243b050f0c05b0163b0be97e
Yiying Zou <yiyingzou@gmail.com>
2019-12-09 12:40:10 -0500

    added a missing comma

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       noGifs: false,
       showButton: false,
       errorMessage: false,
-      showLoadingScreen: false
+      showLoadingScreen: false,
       keywordsInArray: false
     };
   }
