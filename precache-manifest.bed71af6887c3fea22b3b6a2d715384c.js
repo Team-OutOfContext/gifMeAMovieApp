90f8407670df4849958a8fc20ac65572482cd498
Yiying Zou <yiyingzou@gmail.com>
2019-12-09 16:58:20 -0500

    Updates

---
+++ precache-manifest.bed71af6887c3fea22b3b6a2d715384c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0282ea80eb9aba44613a9d97ad1a095",
+    "url": "/movieSpoilerProjectSix/index.html"
+  },
+  {
+    "revision": "f5325799a033f2f3135e",
+    "url": "/movieSpoilerProjectSix/static/css/main.4ba86e52.chunk.css"
+  },
+  {
+    "revision": "e7db6c25fbef36d3c78c",
+    "url": "/movieSpoilerProjectSix/static/js/2.72ad15d5.chunk.js"
+  },
+  {
+    "revision": "f5325799a033f2f3135e",
+    "url": "/movieSpoilerProjectSix/static/js/main.8c61db10.chunk.js"
+  },
+  {
+    "revision": "2340918b13cd2dfcf5e3",
+    "url": "/movieSpoilerProjectSix/static/js/runtime-main.0513148d.js"
+  }
+]);
