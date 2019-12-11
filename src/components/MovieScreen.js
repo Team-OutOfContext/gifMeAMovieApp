42bebd0ba8ff316e942a57082480419d67cb26af
Yiying Zou <yiyingzou@gmail.com>
2019-12-10 20:16:30 -0500

    added placeholder alt text & tagline for no keywords

--- src/components/MovieScreen.js
+++ src/components/MovieScreen.js
@@ -23,56 +23,58 @@ class MovieScreen extends Component {
     };
     return (
       <section className="results wrapper">
-          <div className="top-title">
-            <h5 className="now-playing-title">Now Playing</h5>
-            <h3 className="movie-title">{this.props.movieTitle}</h3>
+        <div className="top-title">
+          <h5 className="now-playing-title">Now Playing</h5>
+          <h3 className="movie-title">{this.props.movieTitle}</h3>
+        </div>
+
+        <div className="movie-screen">
+          <div className="movie-poster">
+            <img
+              src={this.props.movieImageUrl}
+              alt={this.props.movieImageAltText}
+            />
           </div>
-
-          <div className="movie-screen">
-            <div className="movie-poster">
-              <img
-                src={this.props.movieImageUrl}
-                alt={this.props.movieImageAltText}
-                />
-            </div>
-            <div className="gif-results">
-
-              <Slider {...settings}>
-                {this.props.gifDataArray.map((gif, i) => {
-                  return (
-                    <li key={i} className="carousel-cell">
-                      <img
-                        className="carousel-cell-image"
-                        src={gif.images.original.webp}
-                        alt={gif.title}
-                      />
-                    </li>
-                  );
-                })}
-              </Slider>
-            </div>
+          <div className="gif-results">
+            <Slider {...settings}>
+              {this.props.gifDataArray.map((gif, i) => {
+                return (
+                  <li key={i} className="carousel-cell">
+                    <img
+                      className="carousel-cell-image"
+                      src={gif.images.original.webp}
+                      alt={gif.title}
+                    />
+                  </li>
+                );
+              })}
+            </Slider>
           </div>
+        </div>
 
-          <div className="critics-review">
-            {this.props.movieKeywords.length === 3 ? (
-              <p>
-                When <strong>{this.props.movieKeywords[0].name}</strong> and
-            <strong> {this.props.movieKeywords[1].name}</strong> fall in love, <strong>{this.props.movieKeywords[2].name}</strong> ensues.
-              </p>
-            ) : null}
-            {this.props.movieKeywords.length === 2 ? (
+        <div className="critics-review">
+          {this.props.movieKeywords.length === 3 ? (
             <p>
               When <strong>{this.props.movieKeywords[0].name}</strong> and
-            <strong> {this.props.movieKeywords[1].name}</strong> fall in love.
-              </p>
-            ) : null}
-            {this.props.movieKeywords.length === 1 ? (
+              <strong> {this.props.movieKeywords[1].name}</strong> fall in love,{" "}
+              <strong>{this.props.movieKeywords[2].name}</strong> ensues.
+            </p>
+          ) : null}
+          {this.props.movieKeywords.length === 2 ? (
+            <p>
+              When <strong>{this.props.movieKeywords[0].name}</strong> and
+              <strong> {this.props.movieKeywords[1].name}</strong> fall in love.
+            </p>
+          ) : null}
+          {this.props.movieKeywords.length === 1 ? (
             <p>
               When <strong>{this.props.movieKeywords[0].name}</strong> ensues.
-              </p>
-            ) : null}
-          </div>
-
+            </p>
+          ) : null}
+          {this.props.movieKeywords.length === 0 ? (
+            <p>Critics say this movie is an indescribable experience.</p>
+          ) : null}
+        </div>
       </section>
     );
   }
